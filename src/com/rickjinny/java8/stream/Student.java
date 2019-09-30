b90b58703b6b9c34983716178764471dc94a46f3
RickJinny <yweijin@163.com>
2019-09-30 13:02:49 +0800

    update stream
    Java8 Stream

---
+++ src/com/rickjinny/java8/stream/Student.java
@@ -0,0 +1,65 @@
+package com.rickjinny.java8.stream;
+
+public class Student {
+
+    // 学生学号
+    public int id;
+
+    // 学生姓名
+    public String name;
+
+    // 学生年龄
+    public int age;
+
+    // 学生性别
+    public String sex;
+
+    public Student(int id, String name, int age, String sex) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
+}
