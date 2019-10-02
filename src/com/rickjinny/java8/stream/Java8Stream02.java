7544215f4b09118d89a0630d76041522923ed1bd
RickJinny <rickjinny@126.com>
2019-10-02 23:57:47 +0800

    update
    update

--- src/com/rickjinny/java8/stream/Java8Stream02.java
+++ src/com/rickjinny/java8/stream/Java8Stream02.java
@@ -1,5 +1,13 @@
 package com.rickjinny.java8.stream;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Java8Stream02 {
 
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("AA", "BB", "CC", "DD");
+        Stream<String> stream1 = list.stream();
+    }
 }
