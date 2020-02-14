ddc77a7ed6b3b02fc6b148ba2df7322011ebe30b
RickJinny <rickjinny@126.com>
2020-02-14 23:53:41 +0800

    Stream
    Stream

--- src/com/rickjinny/java8/stream/Java8Stream03.java
+++ src/com/rickjinny/java8/stream/Java8Stream03.java
@@ -20,20 +20,36 @@ public class Java8Stream03 {
         listA.add("66");
 
         List<String> listB = new ArrayList<>();
-        listA.add("44");
-        listA.add("55");
-        listA.add("77");
-        listA.add("88");
-        listA.add("99");
+        listB.add("44");
+        listB.add("55");
+        listB.add("77");
+        listB.add("88");
+        listB.add("99");
 
         /**
-         * 两个 List 集合的交集: listA 与 listB 的交集
+         * 第一、两个 List 集合的交集: listA 与 listB 的交集
          */
         List<String> list1 = listA.stream().filter(item -> listB.contains(item)).collect(Collectors.toList());
-        System.out.println("1、listA 与 listB 的交集");
-        list1.parallelStream().forEach(System.out::print);
+        System.out.println("1、listA 与 listB 的交集: ");
+        list1.parallelStream().forEach(System.out::println);
 
+        /**
+         * 第二、两个 List 集合的差集: listA - listB
+         */
+        List<String> list2 = listA.stream().filter(item -> !listB.contains(item)).collect(Collectors.toList());
+        System.out.println("2、listA 与 listB 的差集: ");
+        list2.parallelStream().forEach(System.out::println);
 
+        /**
+         * 第三、两个 List 集合的差集: listB - listA
+         */
+        List<String> list3 = listB.stream().filter(item -> !listA.contains(item)).collect(Collectors.toList());
+        System.out.println("2、listB 与 listA 的差集: ");
+        list3.parallelStream().forEach(System.out::println);
+
+        /**
+         * 第四、两个 List 集合的并集
+         */
 
     }
 }
